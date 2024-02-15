e64a9a4853e0f2578f2942c9c7dadd1f470ea365
Jackson Lorran <computadordb@gmail.com>
2024-02-15 09:28:15 -0300

    teste

--- index.py
+++ index.py
@@ -4,11 +4,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>testando apenas</h1>'
-@app.route('/teste')
-def home():
     return render_template('index.html')
 
 @app.route('/about')
 def about():
     return 'About'
+app.run(debug=True)
