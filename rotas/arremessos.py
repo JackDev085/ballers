b872ef2ea580eda214f3069e37daa4c243abfe36
Jackson Lorran <computadordb@gmail.com>
2024-02-15 17:14:51 -0300

    problemas de deploy resolvidos

--- rotas/arremessos.py
+++ rotas/arremessos.py
@@ -1,6 +1,7 @@
 from flask import render_template,Blueprint
 arrremessos_route = Blueprint('arremessos',__name__)
 
+
 @arrremessos_route.route('/')
 def listaArremessos():
   return render_template('arremessos.html', title='arremessos',css="css/arremessos.css")
--- rotas/home.py
+++ rotas/home.py
@@ -3,4 +3,4 @@ home_routes = Blueprint('home', __name__)
 
 @home_routes.route('/')
 def home():
-    return render_template('test.html',title='Ballers',css="css/styles.css")
+    return render_template('index.html',title='Ballers',css="css/styles.css")
