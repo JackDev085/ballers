bd5313bea0ad61b3835501dfc74a75460f202198
Jackson Lorran <computadordb@gmail.com>
2024-02-15 16:26:51 -0300

    Resolvendo erros de deploy/1

--- api/app.py
+++ api/app.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from routes.home import home_routes
-from routes.arremessos import arrremessos_route
+# from routes.arremessos import arrremessos_route
 
 app = Flask(__name__)
 app.register_blueprint(home_routes)
-app.register_blueprint(arrremessos_route,url_prefix="/arremessos")
+# app.register_blueprint(arrremessos_route,url_prefix="/arremessos")
 
 
 if __name__ == '__main__':
